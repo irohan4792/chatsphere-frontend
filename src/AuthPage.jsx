a4fb6728c1001f75cf97c36a53283c8c9e2f4308
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2024-01-14 12:19:29 +0530

    url error

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -4,7 +4,7 @@ const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      axios.post('http://localhost:3001/authenticate',
+      axios.post('http://chatspheree.netlify.app/authenticate',
       {username:value}
       ).then(r=>props.onAuth({...r.data, secret:value}))
       .catch(e=> console.log('error', e))
